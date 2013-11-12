93088d0d55c07b393eb1670bdb8cfe0988d97632
3yatsu <ms018a.k@gmail.com>
2013-11-12 20:22:42 +0900

    finish 6.3.2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
 
 describe User do
-	before { @user = User.new(name:"Example User", email:"user@example.com") }
+
+	before do
+    	@user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  	end
 
 	subject { @user }
 
 	it { should respond_to(:name) }
 	it { should respond_to(:email) }
+	it { should respond_to(:password_digest) }
+	it { should respond_to(:password) }
+	it { should respond_to(:password_confirmation) }
 
 	it { should be_valid }
 
@@ -35,32 +42,44 @@ describe User do
 		end
 	end
 
-  describe "when email format is valid" do
-    it "should be valid" do
-      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-      addresses.each do |valid_address|
-        @user.email = valid_address
-        expect(@user).to be_valid
-      end
-    end
-  end
+	  describe "when email format is valid" do
+	    it "should be valid" do
+	      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+	      addresses.each do |valid_address|
+	        @user.email = valid_address
+	        expect(@user).to be_valid
+	      end
+	    end
+	  end
 
-  describe "when email address is already taken" do 
-  	before do
-  		user_with_same_email = @user.dup
-  		user_with_same_email.save
-  	end
+	  describe "when email address is already taken" do 
+	  	before do
+	  		user_with_same_email = @user.dup
+	  		user_with_same_email.save
+	  	end
 
-  	it { should_not be_valid }
-  end
+	  	it { should_not be_valid }
+	  end
 
-  describe "when email address is already taken" do
-  	before do
-  		user_with_same_email = @user.dup
-  		user_with_same_email.email = @user.email.upcase
-  		user_with_same_email.save
-  	end
+	  describe "when email address is already taken" do
+	  	before do
+	  		user_with_same_email = @user.dup
+	  		user_with_same_email.email = @user.email.upcase
+	  		user_with_same_email.save
+	  	end
 
-  	it { should_not be_valid }
-  end
+	  	it { should_not be_valid }
+	  end
+	  describe "when password is not present" do
+		before do
+		  @user = User.new(name: "Example User", email: "user@example.com",
+		                   password: " ", password_confirmation: " ")
+		end
+		it { should_not be_valid }
+	  end
+
+	  describe "when password doesn't match confirmation" do
+	    before { @user.password_confirmation = "mismatch" }
+	    it { should_not be_valid }
+	  end
 end
